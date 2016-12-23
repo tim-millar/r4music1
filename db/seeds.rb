bc3f4ac9a3f051f1aca938a6a69034464694e7ce
Tim Millar <heteroskedasticity99@gmail.com>
2016-12-23 20:19:16 +0000

    Created db seed task.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Composer.destroy_all
+Work.destroy_all
+Edition.destroy_all
+
+composers = [
+  { first_name: 'Johannes', last_name: 'Brahms' },
+  { first_name: 'Claude', last_name: 'Debussy' },
+]
+
+Composer.create!(composers)
+
+works = [
+  { title: 'Sonata for Cello and Piano in F Major' },
+  { title: 'String Quartet' },
+]
+
+Work.create!(works)
+
+editions = [
+  { description: 'Facsimilie', publisher: 'Black Music House', year: 1998, price: 21.95 },
+  { description: 'Urtext', publisher: 'RubyTunes', year: 1977, price: 17.99 },
+  { description: 'Ed. Y.Matsumo', publisher: 'RubyTunes', year: 2001, price: 23.50 },
+  { description: '', publisher: 'Black Music House', year: 1995, price: 39.95 },
+  { description: 'Reprint', publisher: 'RubyTunes', year: 2011, price: 31.99 },
+]
+
+Edition.create!(editions)
+
+brahms = Composer.where(last_name: 'Brahms').first
+debussy = Composer.where(last_name: 'Debussy').first
+
+sonata = Work.where(works[0]).first
+strings = Work.where(works[1]).first
+
+brahms.works << sonata
+debussy.works << strings
+
+Edition.all.map.with_index do |edition, idx|
+  if idx % 2 == 0
+    sonata.editions << edition
+  else
+    strings.editions << edition
+  end
+end
